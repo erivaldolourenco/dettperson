6f408e3d79c4820fe2e455487e88926b7661c534
Erivaldo Lourenço <erivaldolourenco.m@gmail.com>
2022-03-20 16:29:06 -0300

    Iniciando repositório.

---
+++ src/main/java/br/com/dett/dettperson/DettPersonApplication.java
@@ -0,0 +1,13 @@
+package br.com.dett.dettperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DettPersonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DettPersonApplication.class, args);
+    }
+
+}
